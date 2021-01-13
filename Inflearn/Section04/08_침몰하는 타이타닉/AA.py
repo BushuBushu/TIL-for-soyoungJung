c6f44f91b46d1e068efb760015a245a1030ecae4
soyoung <so_young_jung@naver.com>
2021-01-14 00:03:19 +0900

    ADD: Section 04 8번 풀이 추가

--- Inflearn/Section04/08_침몰하는 타이타닉/AA.py
+++ Inflearn/Section04/08_침몰하는 타이타닉/AA.py
@@ -42,3 +42,28 @@ else:
     cnt += 2
     
 print(cnt)
+
+# 강의 풀이
+#deque사용하기
+#froom collections import deque
+n, limit = map(int, input().split())
+p = list(map(int, input().split()))
+p.sort()
+#p = deque(p)
+cnt = 0
+while p:
+    # p에 마지막 한명이 남은 경우를 생각해야 함.
+    if len(p) == 1:
+        cnt += 1
+        break
+    if p[0] + p[-1] > limit:
+        p.pop()
+        cnt += 1
+    else:
+        #p.popleft()
+        p.pop(0)
+        p.pop()
+        cnt += 1
+print(cnt)
+
+#deque사용
