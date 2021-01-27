244d1f72b59b66430b8e24492b31eda91fdd5660
soyoung <so_young_jung@naver.com>
2021-01-27 16:02:25 +0900

    MOD: Section05 6번 문제 추가

--- Inflearn/Section05/06_응급실/AA.py
+++ Inflearn/Section05/06_응급실/AA.py
@@ -24,3 +24,23 @@ while True:
         chkdq.popleft()
         res += 1
     
+#강의 풀이
+import sys
+from collections import deque
+sys.stdin=open("input.txt", "r")
+n, m=map(int, input().split())
+#pos는 인덱스, val은 위험도가 됨. 튜플로 이루어진 리스트인 Q
+Q=[(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+Q=deque(Q)
+cnt=0
+while True:
+    cur=Q.popleft()
+    # cur[1]은 위험도를 의미. Q에 존재하는 x[1]값, 즉 현재 분석 중인 환자의 위험도가 가장 높은 것이 아니라면, 뒤로 보내기
+    if any(cur[1]<x[1] for x in Q):
+        Q.append(cur)
+    # 가장 높은 위험도면 치료하기
+    else:
+        cnt+=1
+        if cur[0]==m:
+            print(cnt)
+            break
