c5dd4d85ddb8bfd0eceb2170309e8e2f7765ab86
soyoung <so_young_jung@naver.com>
2021-03-11 21:30:14 +0900

    MOD: itertools method 수정

--- Inflearn/Section06/13_라이브러리를 이용한 조합/AA.py
+++ Inflearn/Section06/13_라이브러리를 이용한 조합/AA.py
@@ -5,7 +5,7 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 m = int(input())
 cnt = 0
-for x in it.combination(a, k):
+for x in it.combinations(a, k):
     if sum(x) % m == 0:
         cnt += 1
 
